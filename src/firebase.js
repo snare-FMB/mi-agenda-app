d0c45d968d213bcbc3934ffb7b01898fc4fd1b34
jorge <jorge@localhost.localdomain>
2025-12-09 09:43:32 -0600

    Actualizar configuración de Firebase a nuevo proyecto

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,17 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyACkAMSoQ_-iABpAxwKWdKN8ttGY2uXRRc",
-  authDomain: "mi-agenda-app-3d047.firebaseapp.com",
-  projectId: "mi-agenda-app-3d047",
-  storageBucket: "mi-agenda-app-3d047.firebasestorage.app",
-  messagingSenderId: "659150244676",
-  appId: "1:659150244676:web:0fa29ed15ce046bd296511"
+  apiKey: "AIzaSyAiXhc7B3nup_T1yrZef0zemiT5_t4_8hQ",
+  authDomain: "mi-agenda-app-v2.firebaseapp.com",
+  projectId: "mi-agenda-app-v2",
+  storageBucket: "mi-agenda-app-v2.firebasestorage.app",
+  messagingSenderId: "301078194068",
+  appId: "1:301078194068:web:587f5d8f4a71e34c6e9181"
 };
-      
-
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
